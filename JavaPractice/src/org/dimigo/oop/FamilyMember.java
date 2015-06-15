00e37e36ae9d7b09c660fab243c35f98f0247f1b
shift <shift@naver.com>
2015-06-15 09:27:13 +0900

    .

---
+++ JavaPractice/src/org/dimigo/oop/FamilyMember.java
@@ -0,0 +1,21 @@
+package org.dimigo.oop;
+
+public class FamilyMember {
+
+	private static int memberCnt;
+	private String memberName;
+	
+	public FamilyMember(String memberName) {
+		this.memberName = memberName;
+		memberCnt++;
+	}
+	
+	public String getMemberName() {
+		return memberName;
+	}
+	
+	public static void printMemberCnt() {
+		System.out.println("가족 총 인원 수 : " + memberCnt + "명");
+	}
+	
+}
